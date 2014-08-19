fd51d5394990909a4e4334430ac198710acc1d49
superhao <zipu888@aliyun.com>
2014-08-19 16:36:22 +0800

    modify add memchache

--- src/main/java/com/xiaohao/cms/controller/IndexController.java
+++ src/main/java/com/xiaohao/cms/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.xiaohao.cms.controller;
 
+import com.xiaohao.cms.model.Pages;
+import com.xiaohao.cms.service.CmsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by xiaohao on 2014/8/18.
@@ -10,5 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class IndexController {
+
+
+
+    @Autowired
+    CmsService cmsService;
+
+    @RequestMapping(method = RequestMethod.GET,value = "front/index")
+    public List<Pages> index(){
+
+        List<Pages> rootMenus =cmsService.queryForRootMenu();
+
+        return rootMenus;
+    }
 
 }
--- src/main/java/com/xiaohao/cms/service/CmsService.java
+++ src/main/java/com/xiaohao/cms/service/CmsService.java
@@ -3,10 +3,14 @@ package com.xiaohao.cms.service;
 import com.xiaohao.cms.mapper.AdminMapper;
 import com.xiaohao.cms.model.Admin;
 import com.xiaohao.cms.model.AdminExample;
+import com.xiaohao.cms.model.Pages;
+import com.xiaohao.cms.model.PagesExample;
+import org.apache.derby.iapi.store.raw.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by xiaohao on 2014/8/18.
@@ -19,5 +23,11 @@ public class CmsService extends BaseService {
 
     }
 
+
+    public List<Pages> queryForRootMenu(){
+        PagesExample pagesExample = new PagesExample();
+        pagesExample.createCriteria().andIsmenuNotEqualTo(false).andIssecondarymenuEqualTo(true);
+        return pagesMapper.selectByExample(pagesExample);
+    }
 
 }
