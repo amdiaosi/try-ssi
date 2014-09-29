e901e7666c8e6f288b38a2eaa66915b02fb35375
superhao <zipu888@aliyun.com>
2014-09-29 10:43:22 +0800

    modify

--- src/main/java/com/xiaohao/cms/MQ/MQLogEvent.java
+++ src/main/java/com/xiaohao/cms/MQ/MQLogEvent.java
@@ -1,6 +1,7 @@
 package com.xiaohao.cms.MQ;
 
 import com.lmax.disruptor.EventHandler;
+import com.xiaohao.cms.service.MQHandler;
 
 /**
  * Created by xiaohao on 2014/9/28.
@@ -8,7 +9,10 @@ import com.lmax.disruptor.EventHandler;
 public class MQLogEvent<T> {
 
     private T data;
-
+    //我们可以再这个event上面添加handler
+    //队列接收到event后 取得data 和操作data的handler
+    //这样这个队列就可以异步操作不用的数据
+    //private MQHandler;
 
     public T getData() {
         return data;
--- src/main/java/com/xiaohao/cms/service/MQServcie.java
+++ src/main/java/com/xiaohao/cms/service/MQServcie.java
@@ -7,51 +7,63 @@ import com.lmax.disruptor.dsl.ProducerType;
 import com.xiaohao.cms.MQ.LogEventFactory;
 import com.xiaohao.cms.MQ.MQLogEvent;
 import com.xiaohao.cms.model.AccessLog;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
+ * <p>使用disruptor来实现异步的日志记录</p>
+ *
+ * disruptor 是一个本地系统的异步操作框架
+ *
  * Created by xiaohao on 2014/9/28.
  */
 public class MQServcie {
 
+    //注入我们的事件处理器 用来处理队列中的数据
     @Resource(name = "eventHandler")
     MQHandler mqHandler;
 
     private int bufferSize = 8192;
-    //private StatEventProducer producer;
+
     private Disruptor<MQLogEvent> disruptor;
+
     RingBuffer<MQLogEvent> ringBuffer=null;
 
+    /**
+     * 用来初始化圈形的队列
+     */
     private void init(){
+
         Executor executor = Executors.newCachedThreadPool();
-        // The factory for the event
+        // 日志事件生成工厂
         LogEventFactory factory = new LogEventFactory();
-        // Construct the Disruptor
+        // 创建disruptor 需要制定日志的生成工程
         disruptor = new Disruptor<MQLogEvent>(factory, bufferSize, executor, ProducerType.SINGLE,
                 new SleepingWaitStrategy());
-        // Connect the handler
+        // 生成事件处理器
        disruptor.handleEventsWith(mqHandler);
-        // Start the Disruptor, starts all threads running
+        // 启动disruptor
         ringBuffer = disruptor.start();
         // Get the ring buffer from the Disruptor to be used for publishing.
        // RingBuffer<MQLogEvent> ringBuffer = disruptor.getRingBuffer();
     }
 
     public void putData(AccessLog accessLog){
+        //得到下一个序列
         long sequence = ringBuffer.next(); // Grab the next sequence
         try {
+            //得到序列的节点
             MQLogEvent event = ringBuffer.get(sequence); // Get the entry in the Disruptor
+            //放入需要处理的data
             event.setData(accessLog); // Fill with data
         } finally {
+            //发布出去
             ringBuffer.publish(sequence);
         }
     }
 
+    //停掉disruptor
     public void destroy() {
         if (disruptor != null) {
             disruptor.shutdown();
